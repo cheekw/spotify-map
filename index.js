d5d35ef5811d602859f5db1c578f43b6bfc50881
Viktor Chee <kweic8@gmail.com>
2019-05-30 23:37:49 -0700

    fade animation completed

--- index.js
+++ index.js
@@ -24,68 +24,112 @@ window.onload = function () {
   drawMap(color, svg, tooltip);
 }
 
+// draws the entire choropleth map
 function drawMap(color, svg, tooltip) {
   let path = d3.geoPath();
-
-  d3.csv("./data/artist_ranks.csv").then(rankData => {
-    let ranks = {};
-    for (let i = 0; i < rankData.length; i++) {
-      ranks[rankData[i]['State']] = rankData[i];
-    }
-    d3.json("./data/us-state-names.json").then(states => {
-      d3.json("./data/us-map.json").then(us => {
-        svg.append("g")
-          .attr("class", "states")
-          .selectAll("path")
-          .data(topojson.feature(us, us.objects.states).features)
-          .enter()
-          .append("path")
-          .attr("d", path)
-          .attr("class", (d) => {
-            if (isNotDC(d.id)) {
-              return states[parseInt(d.id)][0];
-            }
-          })
-          .style('stroke-width', "1")
-          .style('stroke', "#fff")
-          .style("fill", (d) => {
-            if (isNotDC(d.id)) {
-              return color(ranks[states[parseInt(d.id)][0]]['US Rank']);
-            }
-          })
-          .on("click", (d) => {
-            if (isNotDC(d.id)) {
-              // TODO: add animation on click
-
-            }
-          })
-          .on("mousemove", (d) => {
-            if (isNotDC(d.id)) {
-              tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-              tooltip.html(
-                "<b>" + "State: " + states[parseInt(d.id)][1] + "</b>" + "<br/>" +
-                "Artist: " + ranks[states[parseInt(d.id)][0]]['Name'] + "<br/>" +
-                "US Rank: " + ranks[states[parseInt(d.id)][0]]['US Rank'] + "<br/>" +
-                "State Rank: " + ranks[states[parseInt(d.id)][0]]['State Rank']
-              )
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY + 25) + "px");
-            }
-          })
-          .on("mouseout", (d) => {
-            tooltip.transition()
-              .duration(500)
-              .style("opacity", 0);
-          });
-
-        svg.append("path")
-          .attr("class", "state-borders")
-          .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => { return a !== b; })));
+  d3.csv("./data/artist_ranks.csv").then(rankData => getStates(rankData))
+    .then(ranks => {
+      d3.json("./data/us-state-names.json").then(states => {
+        d3.json("./data/us-map.json").then(us => {
+          drawChoropleth(color, svg, tooltip, path, ranks, states, us);
+          unfadeMap();
+        });
       });
     });
+}
+
+// adds the choropleth map
+function drawChoropleth(color, svg, tooltip, path, ranks, states, us) {
+  svg.append("g")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", (d) => {
+      if (isNotDC(d.id)) {
+        return "states " + states[parseInt(d.id)][0];
+      }
+    })
+    .style('stroke-width', "1")
+    .style('stroke', "#fff")
+    .style("fill", (d) => {
+      if (isNotDC(d.id)) {
+        return color(ranks[states[parseInt(d.id)][0]]['US Rank']);
+      }
+    })
+    .on("click", (d) => {
+      if (isNotDC(d.id)) {
+        fadeAnimation(states, d.id);
+      }
+    })
+    .on("mousemove", (d) => {
+      if (isNotDC(d.id)) {
+        tooltip.transition()
+          .duration(200)
+          .style("opacity", .9);
+        tooltip.html(
+          "<b>" + "State: " + states[parseInt(d.id)][1] + "</b>" + "<br/>" +
+          "Artist: " + ranks[states[parseInt(d.id)][0]]['Name'] + "<br/>" +
+          "US Rank: " + ranks[states[parseInt(d.id)][0]]['US Rank'] + "<br/>" +
+          "State Rank: " + ranks[states[parseInt(d.id)][0]]['State Rank']
+        )
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY + 25) + "px");
+      }
+    })
+    .on("mouseout", (d) => {
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
+  svg.append("path")
+    .attr("class", "state-borders")
+    .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => { return a !== b; })));
+}
+
+function getStates(rankData) {
+  let ranks = {};
+  for (let i = 0; i < rankData.length; i++) {
+    ranks[rankData[i]['State']] = rankData[i];
+  }
+  return ranks
+}
+
+// unfades the map
+function unfadeMap() {
+  d3.select("html").on("click", () => {
+    let outside = d3.selectAll(".states").filter(equalToEventTarget).empty();
+    if (outside) {
+      unfadeAnimation();
+    }
   });
+}
+
+// gets current click target
+function equalToEventTarget() {
+  return this == d3.event.target;
+}
+
+// creates animtion
+function fadeAnimation(states, id) {
+  for (stateId in states) {
+    if (states[stateId][0] != states[parseInt(id)][0]) {
+      d3.select("." + states[stateId][0])
+        .transition()
+        .style("opacity", 0.1);
+    }
+  }
+  d3.select("." + states[parseInt(id)][0])
+    .transition()
+    .style("opacity", 1);
+}
+
+// creates unfade animation
+function unfadeAnimation(states) {
+  d3.selectAll(".states")
+    .transition()
+    .style("opacity", 1);
 }
 
 // gets rank dictionary from an array, converting the state into a key
